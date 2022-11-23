c4b09144724c969e171d4a43921fb9f563617627
ishraque-dev <ahamedesrak3@gmail.com>
2022-11-24 01:05:39 +0600

    refactoring with mvc and oop

---
+++ src/js/model.js
@@ -0,0 +1,29 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id, throwError) {
+  try {
+    const response = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await response.json();
+    console.log(data);
+
+    if (!response.ok) throwError(response, data);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
